827334122c8a220ebd9eadd8f2ba1bf638a7a1c5
Jannick Brändel <jannick2@braendel.de>
2023-09-24 21:14:43 +0200

    init project

---
+++ src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { CounterComponent } from './counter/counter.component';
+import { BoxesComponent } from './boxes/boxes.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CounterComponent,
+    BoxesComponent
+  ],
+  imports: [
+    BrowserModule
+    //MatButtonModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
