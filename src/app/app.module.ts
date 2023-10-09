0da78df06721c98ef543de686b1d3875eafcff35
Jannick Brändel <jannick2@braendel.de>
2023-10-09 23:01:22 +0200

    Merge branch 'main' of https://github.com/jannickbraendel/sdr-quiz-points into main

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,9 @@ import { CounterComponent } from './counter/counter.component';
 import { BoxesComponent } from './boxes/boxes.component';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatButtonToggleModule } from '@angular/material/button-toggle';
-import { MatFormFieldModule } from '@angular/material/form-field'
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInput, MatInputModule } from '@angular/material/input';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -18,7 +20,10 @@ import { MatFormFieldModule } from '@angular/material/form-field'
     BrowserModule,
     MatDialogModule,
     MatButtonToggleModule,
-    MatFormFieldModule
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
